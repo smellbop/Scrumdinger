5c1ace11a51c9b0678775d7d65b3b7ebd05d3f01
Elliot Pollard <example@example.com>
2025-06-03 13:13:41 +0100

    wtf2

--- Scrumdinger/Models/Attendee.swift
+++ Scrumdinger/Models/Attendee.swift
@@ -1,6 +1,17 @@
-struct Attendee: Identifiable {
-        let id: UUID
+//
+//  Attendee.swift
+//  Scrumdinger
+//
+//  Created by Elliot Pollard on 30/05/2025.
+//
+import Foundation
+import SwiftData
+
+@Model
+class Attendee: Identifiable {
+        var id: UUID
         var name: String
+        var dailyScrum: DailyScrum?
         
         init(id: UUID=UUID(), name:String){
             self.id = id
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -6,10 +6,14 @@
 //
 import Foundation
 import ThemeKit
+import SwiftData
 
-struct DailyScrum: Identifiable{
-    let id: UUID
+@Model
+class DailyScrum: Identifiable{
+    var id: UUID
     var title: String
+    
+    @Relationship(deleteRule: .cascade, inverse: \Attendee.dailyScrum)
     var attendees: [Attendee]
     var lengthInMinutes: Int
     var lengthInMinutesAsDouble: Double {
@@ -21,6 +25,8 @@ struct DailyScrum: Identifiable{
         }
     }
     var theme: Theme
+    
+    @Relationship(deleteRule: .cascade, inverse: \History.dailyScrum)
     var history: [History] = []
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme){
@@ -29,22 +35,6 @@ struct DailyScrum: Identifiable{
         self.attendees = attendees.map { Attendee(name: $0) }
         self.lengthInMinutes = lengthInMinutes
         self.theme = theme
-    }
-}
-
-extension DailyScrum {
-    struct Attendee: Identifiable {
-        let id: UUID
-        var name: String
-        
-        init(id: UUID=UUID(), name:String){
-            self.id = id
-            self.name = name
-        }
-    }
-    
-    static var emptyScrum: DailyScrum {
-        DailyScrum(title: "", attendees: [], lengthInMinutes: 5,theme: .sky)
     }
 }
 
